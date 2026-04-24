bfa15b218c026293f18ba86d80186dbf73c64a44
ZuBoDoggo <43576@pt.ac.th>
2026-04-25 02:59:09 +0700

    set up Atrraction structure

--- src/app/hotels/[hotelId]/attractions/page.tsx
+++ src/app/hotels/[hotelId]/attractions/page.tsx
@@ -2,6 +2,8 @@ export default function (){
     return (
         <div>
             Admiin Attraction View : To be Implemented
+            <br></br>
+            PS. I know the button is a bit off, let's fix it later
         </div>
     );
 }
