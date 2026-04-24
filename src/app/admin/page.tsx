65e648413106256f6a1103a3a09ac234178e95fc
ZuBoDoggo <43576@pt.ac.th>
2026-04-25 01:37:33 +0700

    rewind atrraction

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -6,23 +6,16 @@ export default function AdminPage() {
       <div className="mb-8">
         <h1 className="text-4xl font-bold">Admin Dashboard</h1>
         <p className="mt-2 text-gray-500">
-          Manage hotels, attraction, bookings, and transport services.
+          Manage hotels, bookings, and transport services.
         </p>
       </div>
 
-      <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-2">
+      <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
         <Link
           className="flex min-h-48 p-10 flex-col items-center justify-center rounded-xl bg-secondary border-2 border-primary shadow-sm shadow-black text-center text-balance text-lg font-bold transition hover:-translate-y-1 dark:border-none dark:bg-dark-primary dark:text-white dark:hover:bg-dark-primary-0"
           href="/admin/hotels"
         >
           Add Hotels
-        </Link>
-
-        <Link
-          className="flex min-h-48 p-10 flex-col items-center justify-center rounded-xl bg-secondary border-2 border-primary shadow-sm shadow-black text-center text-balance text-lg font-bold transition hover:-translate-y-1 dark:border-none dark:bg-dark-primary dark:text-white dark:hover:bg-dark-primary-0"
-          href="/admin/attraction"
-        >
-          Manage Attraction
         </Link>
 
         <Link
