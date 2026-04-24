56f1474a99abbc7c80392bb58ca2a91b8c66b2a6
Chris Sophos <chris.sophos977@gmail.com>
2026-04-24 11:59:01 +0700

    add detailed pagination interface

--- src/interface/ApiResponse.ts
+++ src/interface/ApiResponse.ts
@@ -1,4 +1,14 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 export type ApiResponse<T, Extra = {}> = { success: true; data: T; } & Extra | { success: false; message: string; }
 
-export type PaginatedApiResponse<T> = ApiResponse<T, { total: number }>
+interface Page {
+  page: number;
+  limit: number;
+}
+
+interface Pagination {
+  next?: Page;
+  prev?: Page;
+}
+
+export type PaginatedApiResponse<T> = ApiResponse<T, { total: number; pagination: Pagination; }>
